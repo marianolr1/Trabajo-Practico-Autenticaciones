8f7abb2ee1d6f35250ba6e4a724fca36a0c8d92f
Pablo <Pablo@PABLO-HP>
2016-09-29 18:14:14 -0300

    listo

--- Trabajo Practico Autenticaciones/src/Servidor/Servidor.java
+++ Trabajo Practico Autenticaciones/src/Servidor/Servidor.java
@@ -2,14 +2,5 @@ package Servidor;
 
 public class Servidor {
 	public void prueba(){
-<<<<<<< HEAD
-		//comentario pablo
-		System.out.println("Holavaaaaaaagggg");
-		//comentario mariano
-		
-		////comentario mariano 4545626
-=======
-		//comentario mio
->>>>>>> branch 'master' of https://github.com/marianolr1/Trabajo-Practico-Autenticaciones.git
 	}
 }
