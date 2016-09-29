d89760dc5248c3bdcef672a122d363f2e0f70c12
Mariano <Mariano@MarianoAsus>
2016-09-29 18:15:04 -0300

    yjtyh

--- Trabajo Practico Autenticaciones/src/Servidor/Servidor.java
+++ Trabajo Practico Autenticaciones/src/Servidor/Servidor.java
@@ -2,14 +2,12 @@ package Servidor;
 
 public class Servidor {
 	public void prueba(){
-<<<<<<< HEAD
+
 		//comentario pablo
 		System.out.println("Holavaaaaaaagggg");
 		//comentario mariano
 		
 		////comentario mariano 4545626
-=======
-		//comentario mio
->>>>>>> branch 'master' of https://github.com/marianolr1/Trabajo-Practico-Autenticaciones.git
+
 	}
 }
