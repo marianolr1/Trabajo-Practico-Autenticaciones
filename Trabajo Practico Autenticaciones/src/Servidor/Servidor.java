10496194f94223f397330ac791e08ae941ecd8f5
Puablito <pcassettai@gmail.com>
2016-10-01 20:05:39 -0300

    funcionamiento del socket

--- Trabajo Practico Autenticaciones/src/Servidor/Servidor.java
+++ Trabajo Practico Autenticaciones/src/Servidor/Servidor.java
@@ -1,6 +1,31 @@
 package Servidor;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
-	public void prueba(){
+	public static void main(String[] args) {
+		new Servidor().iniciar();
+	}
+
+	public void iniciar(){
+		ServerSocket servidor = null;
+		Socket socket = null;
+		
+		try {
+			servidor = new ServerSocket(5050); //el puerto lo tenemos que levantar del archivo de configuraciones?
+			System.out.println("Servidor iniciado - esperando conexiones");
+			while (true) {
+				socket = servidor.accept(); //espera de conexiones
+				System.out.println("Conexión desde ip: "+socket.getInetAddress()); // ver de sacarle la "/" del principio de la ip
+				
+				new SocketServidor (socket); //se envia la conexión entrante para generacion de thread
+
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
