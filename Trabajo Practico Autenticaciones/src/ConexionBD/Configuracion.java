47f2b954054da39851d099fd46f554bb6120cd1f
Mariano <Mariano@MarianoAsus>
2016-10-02 22:31:52 -0300

    configConexion

--- Trabajo Practico Autenticaciones/src/ConexionBD/Configuracion.java
+++ Trabajo Practico Autenticaciones/src/ConexionBD/Configuracion.java
@@ -1,12 +1,29 @@
 package ConexionBD;
 
+import java.io.FileInputStream;
 import java.util.Properties;
 
 public class Configuracion {
 
-	private Properties propiedades;
-	public Configuracion() {
-		// TODO Auto-generated constructor stub
+	private static Properties propiedades;
+	private Configuracion() {
+		try {
+			 String rutaArchivo = System.getProperty("user.dir")+System.getProperty("file.separator")
+	         + "configConexion.properties";
+			 FileInputStream is = new FileInputStream(rutaArchivo);
+			 propiedades.load(is);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public static final Properties getInstance(){
+		if (propiedades==null) {
+			propiedades=new Configuracion().propiedades;
+		}
+		return propiedades;
 	}
 
 }
